efcc3ca06126455365623362ba974a5d138e6ce2
Celvin Braun <46591058+CelvinBraun@users.noreply.github.com>
2021-02-24 14:29:21 +0100

    no message

--- life_turtle.py
+++ life_turtle.py
@@ -1,6 +1,47 @@
 from turtle import Turtle
 
-class Life(Turtle):
+START_X_CORR = -350
+START_Y_CORR = 200
+STEPS = 8
+SIZE = 0.3
 
-    def __init__(self):
-        super().__init__()
+class Life:
+
+    def __init__(self, years):
+        self.start_x = START_X_CORR
+        self.start_y = START_Y_CORR
+        self.steps = STEPS
+        self.size = SIZE
+        self.weeks = round(years * 52.1786)
+        self.age = 0
+        self.week_list = []
+
+        self.create_weeks()
+
+    def create_weeks(self):
+
+        x_cor = self.start_x
+        y_cor = self.start_y
+
+        for week in range(self.weeks):
+            new_week = Turtle()
+            new_week.hideturtle()
+            new_week.speed("fastest")
+            new_week.shapesize(self.size)
+            new_week.shape("square")
+            new_week.fillcolor("white")
+            new_week.penup()
+
+            y_cor -= self.steps
+
+            if week % 50 == 0:
+                y_cor = self.start_y
+                x_cor += self.steps
+            new_week.goto(x_cor, y_cor)
+            new_week.showturtle()
+
+            self.week_list.append(new_week)
+
+
+    def fill_weeks(self, age):
+        pass
--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 from life_turtle import Life
+from turtle import Screen
 
+screen = Screen()
+screen.setup(height=500, width=800)
+screen.title("Life in Weeks:")
+screen.tracer(0)
+
+years = screen.textinput(title="Average age of human 79 years", prompt="For different age enter age: ")
+if len(int(years))==0:
+    years = 79
+
+print(len(years))
+#life = Life(years)
+format_not_right = True
+
+screen.update()
+
+while format_not_right:
+    birthday = screen.textinput(title="Your birthday", prompt="Please enter in format: day,month,year:")
+    birthday = birthday.split(",")
+    if len(birthday)==3:
+        format_not_right = False
+
+
+life.fill_weeks()
+
+screen.exitonclick()
